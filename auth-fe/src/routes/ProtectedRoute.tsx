363d9377e64becd22e1e9a65db38d0fa89fb245c
tunang <nguyentuan20042207@gmail.com>
2025-09-04 23:57:38 +0700

    protected route comiit

--- auth-fe/src/routes/ProtectedRoute.tsx
+++ auth-fe/src/routes/ProtectedRoute.tsx
@@ -1,21 +1,21 @@
-// import type { RootState } from "@/store";
-// import { initializeAuth } from "@/store/slices/authSlice";
+import type { RootState } from "@/store";
+import { initializeAuth } from "@/store/slices/authSlice";
 import { type ReactNode } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({allowedRoles, children}: {allowedRoles: string[], children: ReactNode}) => {
-  // const {user, isLoading} = useSelector((state: RootState) => state.auth);
+  const {user, isLoading} = useSelector((state: RootState) => state.auth);
   const dispatch = useDispatch();
 
-  // if (!user && !isLoading) {
-  //   dispatch(initializeAuth());
-  //   return <Navigate to="/login" />;
-  // }
+  if (!user && !isLoading) {
+    dispatch(initializeAuth());
+    return <Navigate to="/login" />;
+  }
 
-  // if (!allowedRoles.includes(user?.role) && !isLoading) {
-  //   return <Navigate to="/" />;
-  // }
+  if (!allowedRoles.includes(user?.role) && !isLoading) {
+    return <Navigate to="/" />;
+  }
 
   return <>{children}</>;
 };
